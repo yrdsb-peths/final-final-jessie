6a310e23f3b0f6bfecea5688fa418a2fb9f13172
Super-JSB <lujessie099@gmail.com>
2026-05-22 21:56:40 -0400

    Update Thief.java

--- Thief.java
+++ Thief.java
@@ -8,10 +8,10 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class Thief extends Actor
 {
-    /**
-     * Act - do whatever the Thief wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    private String direction ="";
+    
+    
+    
     //constructor
     
     public Thief()
@@ -24,6 +24,6 @@ public class Thief extends Actor
     public void act()
     {
         // Add your action code here.
-    
+        if(
     }
 }
