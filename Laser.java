9fb695872783e966ec1a439db9a8c9ce0be15220
Super-JSB <lujessie099@gmail.com>
2026-05-26 18:45:50 -0400

    shoot laser method

--- Laser.java
+++ Laser.java
@@ -15,15 +15,6 @@ public class Laser extends Actor
     public void act()
     {
         // Add your action code here.
-        checkPlayer();
     }
-    public void checkPlayer()
-    {
-        if(isTouching(Thief.class))
-        {
-            Thief thief = (Thief)getOneIntersectingObject(Thief.class);
-            
-            thief.loseLife();
-        }
-    }
+
 }
--- MyWorld.java
+++ MyWorld.java
@@ -1,6 +1,7 @@
 import greenfoot.*;
 
 public class MyWorld extends World {
+    int yellowLaserTimer = 0;
     public MyWorld() {
         super(800, 600, 1);
         
@@ -14,11 +15,30 @@ public class MyWorld extends World {
         
         YellowLaser yL = new YellowLaser();
         addObject(yL, 230,130);
+        yL.setRotation(90);
+        
     }
     
+    public void act()
+    {
+        yellowLaserTimer++;
+        
+        if(yellowLaserTimer >= 120)
+        {
+            shootLaser();
+            yellowLaserTimer = 0;
+        }
+    }
     public void gameOver()
     {
         showText("GAME OVER!", getWidth(), getHeight()/2);
         Greenfoot.stop();
+    }
+    
+       public void shootLaser()
+    {
+        YellowLaser laser = new YellowLaser();
+        addObject(laser, 230, 130);
+        laser.setRotation(90);
     }
 }
