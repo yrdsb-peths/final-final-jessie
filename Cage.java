013e85817db756e23940da17b54f9dd2c310350f
Super-JSB <lujessie099@gmail.com>
2026-06-07 14:23:54 -0400

    Update Cage.java

--- Cage.java
+++ Cage.java
@@ -3,9 +3,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Cage extends Actor
 {
-   
+    
+    public Cage()
+    {
+        GreenfootImage cage = new GreenfootImage("cage.png");
+        cage.setTransparency(100);
+        setImage(cage);
+    }
     public void act()
     {
-        // Add your action code here.
+        
     }
 }
