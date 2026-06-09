77531975a00ec796479abfefcb1b16a7ac172d6f
Super-JSB <lujessie099@gmail.com>
2026-06-09 17:19:32 -0400

    title screen

--- TitleScreen.java
+++ TitleScreen.java
@@ -16,6 +16,14 @@ public class TitleScreen extends World
     public TitleScreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(800, 600, 1); 
+    }
+    
+    public void act()
+    {
+        if(Greenfoot.isKeyDown("space"))
+        {
+            Greenfoot.setWorld(new Level1());
+        }
     }
 }
